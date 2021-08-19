bc1dc3a3d5a0d66fcfaea02e18b4a31b1a4b17f8
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-08-19 18:16:41 +1200

    first commit for ReactJS Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
