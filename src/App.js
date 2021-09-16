d490cc4bd5182d0feaaa210dfdf61485dc77099f
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-09-16 16:10:14 +1200

    added handleSubmit function,  rendering the form input for new user

--- src/App.js
+++ src/App.js
@@ -29,10 +29,23 @@ class App extends React.Component {
   // }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state);
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
