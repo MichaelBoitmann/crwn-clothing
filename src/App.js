756e16dac7d6f80393a7a4717f92497eaf59b410
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-09-16 17:27:38 +1200

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -39,8 +39,6 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
           });
         });
       }
