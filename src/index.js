cbb605db06955132242677fe8946d55d68cbc4e4
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-09-20 17:35:39 +1200

    added redux and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react=redux';
+import { Provider } from 'react-redux';
 
 import store from './redux/store';
 
 import './index.css';
 import App from './App';
  
-
-
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
