a9b98ab89b735efafc0fd692894485129e804679
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-09-28 20:55:32 +1300

    added addItem and removeItem functionality to chekout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,27 +11,27 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-        <div className='cart-dropdown'>
-            <div classsName='cart-items'>
-                {cartItems.length ? (
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} item={cartItem} />
-                    ))
-                ) : (
-                    <span className='empty-message'>Your cart is empty</span>                
-                )}
-            </div>
-            <CustomButton onClick={() => {
-                history.push('/checkout');
-                dispatch(toggleCartHidden());
-            }}>
-                GO TO CHECKOUT
-            </CustomButton>
-        </div>
+  <div className='cart-dropdown'>
+    <div classsName='cart-items'>
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+          <span className='empty-message'>Your cart is empty</span>                
+          )}
+    </div>
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden());
+    }}>
+      GO TO CHECKOUT
+    </CustomButton>
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector ({
-    cartItems: selectCartItems
+  cartItems: selectCartItems
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
