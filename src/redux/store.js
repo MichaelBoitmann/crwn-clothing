394a64861baaf70ccf2b47cbcc44b0b909d2b4d3
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-09-30 20:25:03 +1300

    added local storage and session storage for different platform while the tab still on with the help of reducx persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 
+import { persistStore } from 'redux-persist'; // allow the browser to cache the store
+
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
+
+
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
